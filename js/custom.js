e5fb355f6540df54143c0a55f16620fef3512f6f
Dexter <dex.chavez@yahoo.com>
2022-11-26 10:16:23 +0800

    title change

--- js/custom.js
+++ js/custom.js
@@ -35,6 +35,23 @@
 	});
 	
 	$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
+
+	// Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
 	
 	/* ..............................................
     Map Full
